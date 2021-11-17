2116ca005a06a5bf134771b10e3bcd818cb84ee3
t5t8 <tapio.tuomisto@eficode.com>
2021-11-17 20:00:34 +0200

    fmt

--- terraform/environments/keyvault-demo/main.tf
+++ terraform/environments/keyvault-demo/main.tf
@@ -4,14 +4,14 @@ resource "azurerm_resource_group" "keyvaultdemo" {
 }
 
 module "keyvault_1" {
-  source = "./../../modules/keyvault"
+  source                      = "./../../modules/keyvault"
   name                        = "${var.prefix}-keyvault"
   location                    = azurerm_resource_group.keyvaultdemo.location
   resource_group_name         = azurerm_resource_group.keyvaultdemo.name
   enabled_for_disk_encryption = true
   tenant_id                   = var.tenant_id
 
-  key_vault_readers = []
+  key_vault_readers        = []
   key_vault_administrators = []
 
 }
