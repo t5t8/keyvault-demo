70a214a039d66d1a35043d9c8e2334ca15a863b1
t5t8 <tapio.tuomisto@eficode.com>
2021-11-17 20:00:26 +0200

    fix names

--- terraform/environments/keyvault-demo/example_vm.tf
+++ terraform/environments/keyvault-demo/example_vm.tf
@@ -18,7 +18,7 @@ resource "tls_private_key" "vm" {
 }
 
 resource "azurerm_key_vault_secret" "vm_private_key" {
-  name         = "${var.prefix}-vm-ssh_private_jey"
+  name         = "${var.prefix}-vm-ssh-private-key"
   value        = tls_private_key.vm.private_key_pem
   key_vault_id = module.keyvault_1.id
   content_type = "VM ssh private key"
@@ -34,21 +34,21 @@ resource "azurerm_ssh_public_key" "vm" {
 resource "azurerm_virtual_network" "vm" {
   name                = "${var.prefix}-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.vm.location
-  resource_group_name = azurerm_resource_group.vm.name
+  location            = azurerm_resource_group.keyvaultdemo.location
+  resource_group_name = azurerm_resource_group.keyvaultdemo.name
 }
 
 resource "azurerm_subnet" "vm" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.vm.name
+  resource_group_name  = azurerm_resource_group.keyvaultdemo.name
   virtual_network_name = azurerm_virtual_network.vm.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "vm" {
   name                = "vm-nic"
-  location            = azurerm_resource_group.vm.location
-  resource_group_name = azurerm_resource_group.vm.name
+  location            = azurerm_resource_group.keyvaultdemo.location
+  resource_group_name = azurerm_resource_group.keyvaultdemo.name
 
   ip_configuration {
     name                          = "internal"
@@ -59,8 +59,8 @@ resource "azurerm_network_interface" "vm" {
 
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = "vm-machine"
-  resource_group_name = azurerm_resource_group.vm.name
-  location            = azurerm_resource_group.vm.location
+  resource_group_name = azurerm_resource_group.keyvaultdemo.name
+  location            = azurerm_resource_group.keyvaultdemo.location
   size                = "Standard_F2"
   admin_username      = "adminuser"
   network_interface_ids = [
