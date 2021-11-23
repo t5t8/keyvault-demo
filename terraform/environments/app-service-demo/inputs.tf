80e07da84c7bd0059d7c76df0cb1a3d3da2c0f37
t5t8 <tapio.tuomisto@eficode.com>
2021-11-23 18:03:52 +0200

    add kv data

--- terraform/environments/app-service-demo/inputs.tf
+++ terraform/environments/app-service-demo/inputs.tf
@@ -2,3 +2,7 @@ data "azurerm_resource_group" "demo" {
   name = var.prefix
 }
 
+data "azurerm_key_vault" "demo" {
+  name = "${var.prefix}-keyvault"
+  resource_group_name = data.azurerm_resource_group.demo.name
+}
